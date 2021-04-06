b26d843a4d90c4c2b1e8149a1407dc8d46cd4c1b
Esu Gabriel <orionesu.dev@gmail.com>
2021-04-06 23:14:43 +0100

    Added Functionalities for Navigation Menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 const navSlide = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav__item');
-    const navLinks = document.querySelectorAll('.nav__list')
+    const navLinks = document.querySelectorAll('.nav__item li');
+    const btn = document.querySelectorAll('.nav-btn')
 
     burger.addEventListener('click', () => {
         // toggle nav
         nav.classList.toggle('nav-active');
+
+        // animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else{
+                link.style.animation = `navLinkFade 0.6s ease forwards ${index / 7 + .4}s`;
+            }
+
+        });
+
+        btn.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else{
+                link.style.animation = `navbtnFade 0.4s ease forwards ${index / 7 + 1.5}s`;
+            }
+
+        });
 
         burger.classList.toggle('toggle');
     })
